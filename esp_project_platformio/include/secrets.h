b899133d457193129018ed987792702293420751
florianBurdairon <florian.burdairon@gmail.com>
2024-12-09 23:37:21 +0100

    clean git + rename folder

--- esp_project_platformio/include/secrets.h
+++
@@ -1,8 +0,0 @@
-//For firebase connection
-#define FIREBASE_DB_URL "https://polytech-iot---coffee-machine-default-rtdb.europe-west1.firebasedatabase.app/"
-#define FIREBASE_API_KEY "AIzaSyD52wnj6jGeoI5EgnA9gkJwyfjXf9GNc0o"
-
-// UUIDs for BLE service and characteristics
-#define SERVICE_UUID "e29834b1-fdbe-4780-84f2-f3a87d0e97fb"
-#define WIFI_CREDENTIAL_UUID "dddbf1dc-63f8-4146-bbee-523b47143be8"
-#define DEVICE_INFO_UUID "f5609179-c8f5-49b9-8f88-f4b86ed493e4"
